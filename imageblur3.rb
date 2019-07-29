0d17fe6eb1e0ed03e8784cec20eaca8f3a3fca30
Alif Momin <alifmomin1234567890@gmail.com>
2019-07-29 14:06:35 +0000

    Final

--- imageblur3.rb
+++ imageblur3.rb
@@ -1,1 +1,36 @@
-print "hello world"
+class Image
+
+  def initialize (pictures)
+    @pictures = pictures
+  end
+
+  def one_coordinates
+      one_coordinates = []
+      @pictures.each_with_index do |row, row_number|
+        row.each_with_index do |picture, col_number|
+          if picture == 1
+            one_coordinates << [row_number, col_number]
+          end
+         end
+       end
+      one_coordinates
+  end
+
+  def blur(distance)
+    ones = one_coordinates
+      @pictures.each_with_index do |row, row_number|
+        row.each_with_index do |picture, col_number|
+          ones.each do |found_row_number, found_col_number|
+
+            if row_number == found_row_number && col_number == found_col_number
+              @pictures[row_number][col_number -1] = 1 unless col_number == 0
+              @pictures[row_number][col_number +1] = 1 unless col_number == 3
+              @pictures[row_number -1][col_number] = 1 unless row_number == 0
+              @pictures[row_number +1][col_number] = 1 unless row_number == 3
+            end
+          end
+        end
+      end
+    end
+  end
+
